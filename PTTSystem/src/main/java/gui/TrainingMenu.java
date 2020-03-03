8d57cf558b8bf661ac95a43b3abe7926f619339b
Xinrui Liu <45984700+573802557@users.noreply.github.com>
2020-03-03 09:46:29 +0000

    Add files via upload
    Training Menu.

--- PTTSystem/src/main/java/gui/TrainingMenu.java
+++ PTTSystem/src/main/java/gui/TrainingMenu.java
@@ -5,6 +5,18 @@
  */
 package gui;
 
+import file_management.ListOfStaff;
+import file_management.Staff;
+import java.awt.BorderLayout;
+
+import java.io.*;
+import javax.swing.*;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author Daniel
@@ -14,8 +26,42 @@ public class TrainingMenu extends javax.swing.JFrame {
     /**
      * Creates new form TrainingMenu
      */
+    
+    public DefaultListModel bufJList2;//bufferList for jList2
+    
+    private DefaultTableModel bufTable;
+    private BufferedWriter bw;
+    private String[] columnNames = {"Professor", "Course"};   //列名
+    
     public TrainingMenu() {
+        
+        bufTable = new DefaultTableModel();
+        String[][] tableVales = {{"", ""}}; //数据
+        bufTable = new DefaultTableModel(tableVales, columnNames);       
+        bufJList2 = new DefaultListModel();
         initComponents();
+        
+        //Read Staff List.
+        ListOfStaff model_staff_list = new ListOfStaff(ListOfStaff.getStaff());
+        jList4.setModel(model_staff_list);
+        
+        //Read Teaching Requirement List.
+        String pathname = "TR_List.txt";
+        try (FileReader reader = new FileReader(pathname);
+             BufferedReader br = new BufferedReader(reader)
+        ) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                bufJList2.addElement(line);
+            }
+            
+            jList2.setModel(bufJList2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        
+
     }
 
     /**
@@ -27,24 +73,20 @@ public class TrainingMenu extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jList1 = new javax.swing.JList<>();
         jScrollPane2 = new javax.swing.JScrollPane();
         jList2 = new javax.swing.JList<>();
-        jButton1 = new javax.swing.JButton();
+        addButton = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jButton2 = new javax.swing.JButton();
+        saveButton = new javax.swing.JButton();
+        jScrollPane5 = new javax.swing.JScrollPane();
+        jTable2 = new javax.swing.JTable();
+        jScrollPane4 = new javax.swing.JScrollPane();
+        jList4 = new javax.swing.JList<>();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Teaching Requests System");
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-
-        jList1.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
-            public int getSize() { return strings.length; }
-            public String getElementAt(int i) { return strings[i]; }
-        });
-        jScrollPane1.setViewportView(jList1);
 
         jList2.setModel(new javax.swing.AbstractListModel<String>() {
             String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
@@ -53,10 +95,10 @@ public class TrainingMenu extends javax.swing.JFrame {
         });
         jScrollPane2.setViewportView(jList2);
 
-        jButton1.setText("Add Training");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        addButton.setText("Add Training");
+        addButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                addButtonActionPerformed(evt);
             }
         });
 
@@ -64,65 +106,126 @@ public class TrainingMenu extends javax.swing.JFrame {
 
         jLabel2.setText("Courses:");
 
-        jButton2.setText("View Training Request");
-        jButton2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        saveButton.setText("Save Training Request");
+        saveButton.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        saveButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                saveButtonActionPerformed(evt);
             }
         });
+
+        jTable2.setModel(bufTable);
+        jScrollPane5.setViewportView(jTable2);
+
+        jList4.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane4.setViewportView(jList4);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
+                .addGap(10, 10, 10)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(44, 44, 44)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(10, 10, 10)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 119, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jButton1)
-                                .addGap(0, 8, Short.MAX_VALUE))
-                            .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)))
+                            .addComponent(saveButton)
+                            .addComponent(addButton)))
                     .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap())
+                .addGap(10, 10, 10)
+                .addComponent(jScrollPane5, javax.swing.GroupLayout.PREFERRED_SIZE, 295, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 10, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(30, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(jLabel2))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                        .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 222, Short.MAX_VALUE)
-                        .addComponent(jScrollPane1))
+                    .addComponent(jScrollPane5, javax.swing.GroupLayout.PREFERRED_SIZE, 260, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton1)
-                        .addGap(33, 33, 33)
-                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(23, 23, 23))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel2))
+                        .addGap(10, 10, 10)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 222, Short.MAX_VALUE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(addButton)
+                                .addGap(20, 20, 20)
+                                .addComponent(saveButton))
+                            .addComponent(jScrollPane4))))
+                .addGap(10, 10, 10))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    private void addButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addButtonActionPerformed
+        
+        if(jList4.getSelectedIndex() == -1 || jList2.getSelectedIndex() == -1){//if nothing in jl_app is selected...  
+            JOptionPane.showMessageDialog(rootPane, "No Selection!", "Error...", 1) ;
+        }
+        else{
+            //Adding selected element to jl_std and jlm_std
+            String proRem = jList4.getSelectedValue();
+            String TRRem = jList2.getSelectedValue();
+            
+            String[] rowValues = {proRem, TRRem};
+            
+            bufTable.addRow(rowValues);
+            jTable2.setModel(bufTable);
+            
+            //bufJList3.addElement(proRem);
+            //bufJList3.addElement(TRRem);
+            
+            //Removing selected element from jl_app and jlm_app
+            //if (bufJList2.getSize() != 0){bufJList2.removeElementAt(TRI);}
+            //jList2.setModel(bufJList2);
+        }
+        
+    }//GEN-LAST:event_addButtonActionPerformed
+            
+    private void saveButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_saveButtonActionPerformed
+        try {
+            saveFile();// TODO add your handling code here:
+        } catch (IOException ex) {
+            Logger.getLogger(TrainingMenu.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_saveButtonActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
+    /**
+     *
+     * @throws IOException
+     */
+    public void saveFile() throws IOException {
+        File file = new File("Training_Request.txt");
+        // 创建文件
+        file.createNewFile();
+        // creates a FileWriter Object
+        FileWriter fw = new FileWriter(file);
+        bw = new BufferedWriter(fw);
+        bw.write(columnNames[0] + "\t" + columnNames[1] +  "\r\n");
+        int rowNumber = bufTable.getRowCount();
+        for (int i=0;i<rowNumber;i++) {
+            for (int j=0;j<2;j++) {
+                if(bufTable.getValueAt(i, j)!=""){
+                    bw.write(bufTable.getValueAt(i, j) + ",");
+                }
+                
+            }
+            bw.write("\r\n");
+        }
+        bw.close();
+    }
 
     /**
      * @param args the command line arguments
@@ -158,15 +261,17 @@ public class TrainingMenu extends javax.swing.JFrame {
             }
         });
     }
-
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton addButton;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JList<String> jList1;
-    private javax.swing.JList<String> jList2;
-    private javax.swing.JScrollPane jScrollPane1;
+    private static javax.swing.JList<String> jList2;
+    private static javax.swing.JList<String> jList4;
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane4;
+    private javax.swing.JScrollPane jScrollPane5;
+    private javax.swing.JTable jTable2;
+    private javax.swing.JButton saveButton;
     // End of variables declaration//GEN-END:variables
 }
